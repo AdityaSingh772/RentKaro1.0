f86f2443d3e34d44ce42e7ba32544dfa8d3f4d4c
unknown <jeetchandak07@gmail.com>
2024-05-24 14:24:32 +0530

    FirstPage-over

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,41 @@
-"use client"
+// "use client"
+// import React from 'react'
+// import Firstpage from './components/Firstpage'
+// import { UserProvider } from '@auth0/nextjs-auth0/client'
+// import { useUser } from '@auth0/nextjs-auth0/client';
+// import Link from 'next/link';
+
+// const page = () => {
+//   const {user,error,isLoading}=useUser();
+//   if(user){
+//     console.log(user)
+//     return (
+//     <div>
+//       <p>Heelo</p>
+//       <Link href="/api/auth/logout">logout</Link>
+//     </div>
+//     )
+    
+//   }
+
+//   if(isLoading){
+//     return(<div>loading...</div>)
+//   }
+//   return (
+    
+//     <div>
+//       <Firstpage/>
+//     </div>
+    
+//   )
+// }
+
+// export default page
 import React from 'react'
-import Firstpage from './components/Firstpage'
-import { UserProvider } from '@auth0/nextjs-auth0/client'
-import { useUser } from '@auth0/nextjs-auth0/client';
-import Link from 'next/link';
 
 const page = () => {
-  const {user,error,isLoading}=useUser();
-  if(user){
-    console.log(user)
-    return (
-    <div>
-      <p>Heelo</p>
-      <Link href="/api/auth/logout">logout</Link>
-    </div>
-    )
-    
-  }
-
-  if(isLoading){
-    return(<div>loading...</div>)
-  }
   return (
-    
-    <div>
-      <Firstpage/>
-    </div>
-    
+    <div>page</div>
   )
 }
 
