cb3fdfb7cdd83ea964521d6caf04e9d68990008f
AdityaSingh772 <workwithracian@gmail.com>
2024-05-24 14:45:46 +0530

    Extra detail form front and back

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import Image from "next/image";
 import Search from "@/components/Search";
 import RentPage from "./rent/page";
+
+
 export default function Home() {
   return (
     <main>
         <Search/>
         <RentPage/>
+        
     </main>
   );
 }
