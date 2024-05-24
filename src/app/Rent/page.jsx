61f42e8227045b339449b446a65351ea05bc3eb3
Dinesh Jangid <spideyyyyy89@gmail.com>
2024-05-24 13:08:16 +0530

    added-functionality

--- src/app/Rent/page.jsx
+++ src/app/Rent/page.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Result from '@/components/Result'
 import ProductList from '@/components/ProductList'
+import Catagories from "@/components/Catagories"
+import Footer from '@/components/Footer'
 
 const RentPage = () => {
   return (
     <div className="container h-screen w-full flex flex-col">
         <Result/>
         <ProductList/>
+        <Catagories/>
+        <Footer/>
     </div>
   )
 }
