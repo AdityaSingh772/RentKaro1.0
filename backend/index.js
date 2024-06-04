2fe5699b8db3335a5df5a25126b94013292d6357
AdityaSingh772 <workwithracian@gmail.com>
2024-06-04 19:34:58 +0530

    uri

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 
 //connection 
 const port = process.env.PORT || 5000
-mongoose.connect(process.env.MONGO_URL).
+mongoose.connect(process.env.MONGO_URI).
 then(()=>{
     console.log("mongoDb connected");
 });
