bc458f12b206af8bb9aee7803ec5784fd10b3940
AdityaSingh772 <workwithracian@gmail.com>
2024-06-05 14:55:41 +0530

    agin auth0 changed

--- src/app/api/auth/[auth0]/route.js
+++ src/app/api/auth/[auth0]/route.js
@@ -1,15 +1,3 @@
 import { handleAuth } from '@auth0/nextjs-auth0';
 
-
-
-export default handleAuth({
-
-    login: handleLogin({
-  
-      returnTo: '/' // Pass in your preferred callback URL
-  
-    })
-  
-  });
-
-  export const GET = handleAuth();
+export const GET = handleAuth();
